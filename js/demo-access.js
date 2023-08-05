79e290c38690c536058643db52ecc6b02c7c2213
Kate-Khon <katerynakhon@gmail.com>
2023-08-05 22:31:49 +0300

    demo-access 80%

--- js/demo-access.js
+++ js/demo-access.js
@@ -1,25 +1,40 @@
+let username = 'Name';
+
 const list = document.querySelectorAll('.main-menu__list-item');
 const sections = document.querySelectorAll('.demo-access-section');
+const btn = document.querySelector('.btn-schedule');
+const scheduleNav = document.querySelector('.schedule-nav');
+const schedule = document.querySelector('.schedule');
+const nameInput = document.getElementById('username');
 
-console.log(sections);
+nameInput.value = username;
+
+const removeActiveClass = () => {
+  for (let ii = 0; ii < list.length; ii++) {
+    list[ii].classList.remove('active');
+    sections[ii].classList.remove('active');
+  }
+}
+
+const addActiveClass = (element) => {
+  element.classList.add('active');
+}
+
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener('click', () => {
-      for (let ii = 0; ii < list.length; ii++) {
-        list[ii].classList.remove('active');
-        sections[ii].classList.remove('active');
-      }
-
-    list[i].classList.add('active');
-    sections[i].classList.add('active');
+    removeActiveClass();
+    addActiveClass(list[i]);
+    addActiveClass(sections[i]);
   });
 }
 
-// list.forEach(element => {
-//   element.addEventListener('click', () => {
-//     list.forEach(unactiveEl => {
-//       unactiveEl.classList.remove('active');
-//     });
+btn.addEventListener('click', () => {
+  removeActiveClass();
+  addActiveClass(scheduleNav);
+  addActiveClass(schedule);
+});
 
-//     element.classList.add('active');
-//   });
-// });
+nameInput.onchange = () => {
+  username = nameInput.value;
+  nameInput.value = username;
+}
