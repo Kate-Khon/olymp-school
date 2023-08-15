8276b20d7d05a16433124d3298f0bc5c54ec8b06
Kate-Khon <katerynakhon@gmail.com>
2023-08-15 23:56:42 +0300

    promotion section with animation

--- js/online-school.js
+++ js/online-school.js
@@ -1,17 +1,21 @@
-const promotionsImgs = document.querySelectorAll('.promotions__list-part--1');
-const promotionsInfo = document.querySelectorAll('.promotions__list-part--2');
+const promotionsItems = document.querySelectorAll('.promotions__list-item');
 
-for (let i = 0; i < promotionsImgs.length; i++) {
-  promotionsImgs[i].addEventListener('click', () => {
-    promotionsImgs[i].classList.remove('active');
-    promotionsInfo[i].classList.add('active');
+promotionsItems.forEach(element => {
+  element.addEventListener('click', () => {
+    element.classList.toggle('active');
   });
+});
+// for (let i = 0; i < promotionsImgs.length; i++) {
+//   promotionsImgs[i].addEventListener('click', () => {
+//     promotionsImgs[i].classList.remove('active');
+//     promotionsInfo[i].classList.add('active');
+//   });
   
-  promotionsInfo[i].addEventListener('click', () => {
-    promotionsImgs[i].classList.add('active');
-    promotionsInfo[i].classList.remove('active');
-  });
-}
+//   promotionsInfo[i].addEventListener('click', () => {
+//     promotionsImgs[i].classList.add('active');
+//     promotionsInfo[i].classList.remove('active');
+//   });
+// }
 
 
 const osList = document.querySelectorAll('.os-list__list-item');
